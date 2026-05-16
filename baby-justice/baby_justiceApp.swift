b64dbedb97b5794cf110ed0be6e56242ccdd6ba4
Filip <92389794+FDuda354@users.noreply.github.com>
2026-05-16 12:53:24 +0200

    Initial Commit

---
+++ baby-justice/baby_justiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  baby_justiceApp.swift
+//  baby-justice
+//
+//  Created by Filip Duda on 16/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct baby_justiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
